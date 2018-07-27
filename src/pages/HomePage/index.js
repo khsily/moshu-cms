13a159bb22a83f3e9effb65f1471367d2474f04a
Hansu Kim <khsilyqwerty@gmail.com>
2018-07-27 18:18:41 +0900

    Home default ui 변경

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { SectionNav } from '../../components';
 
 class HomePage extends Component {
   render() {
     return (
-      <section id='home-page'>
-        HomePage
+      <section id='home-page' className='container'>
+        <SectionNav left='회원관리' />
+        <div style={{ width: '100%', height: 350, border: '1px solid #ccc', marginBottom: 20 }} />
       </section>
     )
   }
